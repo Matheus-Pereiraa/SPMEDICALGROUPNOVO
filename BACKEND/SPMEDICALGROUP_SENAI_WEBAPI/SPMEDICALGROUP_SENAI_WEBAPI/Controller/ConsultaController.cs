ba559c3c4de53e53c485f6c94102429a49f2a5ae
unknown <50374464812>
2021-11-10 10:01:12 -0300

    termenei o controller da consulta

--- BACKEND/SPMEDICALGROUP_SENAI_WEBAPI/SPMEDICALGROUP_SENAI_WEBAPI/Controller/ConsultaController.cs
+++ BACKEND/SPMEDICALGROUP_SENAI_WEBAPI/SPMEDICALGROUP_SENAI_WEBAPI/Controller/ConsultaController.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SPMEDICALGROUP_SENAI_WEBAPI.Domains;
 using SPMEDICALGROUP_SENAI_WEBAPI.Interfaces;
 using SPMEDICALGROUP_SENAI_WEBAPI.Repositories;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -28,10 +30,37 @@ namespace SPMEDICALGROUP_SENAI_WEBAPI.Controller
             _consultaRepository = new ConsultaRepository();
         }
 
+
+        /// <summary>
+        ///  Listas todas as consultas do usuário logado
+        /// </summary>
+        /// <returns>Uma lista de consultas</returns>
+        [Authorize(Roles = "2,3")]
+        [HttpGet("minhas")]
+        public IActionResult ListarMinhas()
+        {
+            try
+            {
+                int idUsuario = Convert.ToInt32(HttpContext.User.Claims.First(c => c.Type == JwtRegisteredClaimNames.Jti).Value);
+
+                return Ok(_consultaRepository.ListarMinhas(idUsuario));
+            }
+            catch (Exception error)
+            {
+                return BadRequest(new
+                {
+                    mensagem = "Não é possível mostrar as presenças se o usuário não estiver logado!",
+                    error
+                });
+            }
+        }
+
         /// <summary>
         /// Lista todas as Consultas existentes
         /// </summary>
         /// <returns>Uma lista de consultas com o status code 200 - Ok</returns>
+        ///  [Authorize(Roles = "2,3")]
+        [HttpGet("minhas")]
         [HttpGet]
         public IActionResult Listar()
         {
@@ -53,6 +82,107 @@ namespace SPMEDICALGROUP_SENAI_WEBAPI.Controller
                 return NotFound("A Consulta informada não existe!");
             }
             return Ok(ConsultaBuscada);
+        }
+
+        /// <summary>
+        /// Cadastra uma Consulta
+        /// </summary>
+        /// <param name="novaConsulta">Consulta a ser cadastrada</param>
+        /// <returns>Um status code 201 - Created</returns>
+        [Authorize(Roles = "1")]
+        [HttpPost]
+        public IActionResult Cadastrar(Consultum novaConsulta)
+        {
+            _consultaRepository.Cadastrar(novaConsulta);
+
+            return StatusCode(201);
+        }
+
+        /// <summary>
+        /// Atualiza uma consulta existente
+        /// </summary>
+        /// <param name="consultaAtualizada">Objeto com as novas informações da Consulta e o id da consulta a ser atualizada</param>
+        /// <returns>Um status code 204 - No content</returns>
+        [Authorize(Roles = "1")]
+        [HttpPut]
+        public IActionResult Atualizar(Consultum consultaAtualizada)
+        {
+            try
+            {
+                Consultum consultaBuscada = _consultaRepository.BuscarPorId(consultaAtualizada.IdConsulta);
+                if (consultaBuscada != null)
+                {
+                    _consultaRepository.Atualizar(consultaAtualizada);
+                    return StatusCode(204);
+                }
+                else
+                {
+                    return BadRequest(new { mensagem = "A Consulta informada não existe" });
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+        /// <summary>
+        /// Deleta uma consulta
+        /// </summary>
+        /// <param name="idConsulta">id da Consulta a ser deletada</param>
+        /// <returns>Um status code 204 - No content</returns>
+        [Authorize(Roles = "1")]
+        [HttpDelete("{idConsulta}")]
+        public IActionResult Deletar(int idConsulta)
+        {
+            _consultaRepository.Deletar(idConsulta);
+
+            return StatusCode(204);
+        }
+
+        /// <summary>
+        /// Atualiza a situação de uma consulta
+        /// </summary>
+        /// <param name="idConsulta">Id da consulta a ser atualizada</param>
+        /// <param name="status">nova situação da consulta</param>
+        /// <returns>Um status code 200 - OK</returns>
+        [Authorize(Roles = "1")]
+        [HttpPatch("status/{idConsulta}")]
+        public IActionResult AlterarStatus(int idConsulta, Situacao status)
+        {
+            try
+            {
+                _consultaRepository.AlterarStatus(idConsulta, status.IdSituacao);
+
+                return StatusCode(204);
+            }
+            catch (Exception error)
+            {
+                return BadRequest(error);
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Atualiza a descrição de uma consulta
+        /// </summary>
+        /// <param name="idConsulta">Id da consulta a ser atualizada</param>
+        /// <param name="descricao">nova descrição da consulta</param>
+        /// <returns>Um status code 200 - OK</returns>
+        [Authorize(Roles = "2")]
+        [HttpPatch("descricao/{idConsulta}")]
+        public IActionResult AdicionarDescricao(int idConsulta, Consultum descricao)
+        {
+            try
+            {
+                _consultaRepository.AddDescricao(idConsulta, descricao.Descricao);
+
+                return StatusCode(204);
+            }
+            catch (Exception error)
+            {
+                return BadRequest(error);
+                throw;
+            }
         }
     }
 }

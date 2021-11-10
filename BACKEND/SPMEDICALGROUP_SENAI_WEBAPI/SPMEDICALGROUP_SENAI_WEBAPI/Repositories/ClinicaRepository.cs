c2e84be50f152897207bffa8a0feb7311523d6aa
unknown <50374464812>
2021-11-10 10:56:28 -0300

    Terminei até a parte dos Controllers dos Medicos, Falta somente os usuarios paciente eu acho

---
+++ BACKEND/SPMEDICALGROUP_SENAI_WEBAPI/SPMEDICALGROUP_SENAI_WEBAPI/Repositories/ClinicaRepository.cs
@@ -0,0 +1,71 @@
+﻿using SPMEDICALGROUP_SENAI_WEBAPI.Context;
+using SPMEDICALGROUP_SENAI_WEBAPI.Domains;
+using SPMEDICALGROUP_SENAI_WEBAPI.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SPMEDICALGROUP_SENAI_WEBAPI.Repositories
+{
+    public class ClinicaRepository : IClinicaRepository
+    {
+        SPmedContext ctx = new SPmedContext();
+        public void Atualizar(Clinica clinicaAtualizada)
+        {
+            Clinica clinicaBuscada = BuscarPorId(clinicaAtualizada.IdClinica);
+            clinicaBuscada.HorariodeAbertura = clinicaAtualizada.HorariodeAbertura;
+            clinicaBuscada.HorariodeFechamento = clinicaAtualizada.HorariodeFechamento;
+
+            if (clinicaAtualizada.NomeFantasia != null)
+            {
+                clinicaBuscada.NomeFantasia = clinicaAtualizada.NomeFantasia;
+            }
+
+            if (clinicaAtualizada.RazaoSocial != null)
+            {
+                clinicaBuscada.RazaoSocial = clinicaAtualizada.RazaoSocial;
+            }
+
+            if (clinicaAtualizada.Cnpj != null)
+            {
+                clinicaBuscada.Cnpj = clinicaAtualizada.Cnpj;
+            }
+
+            if (clinicaAtualizada.Endereco != null)
+            {
+                clinicaBuscada.Endereco = clinicaAtualizada.Endereco;
+            }
+
+            ctx.Clinicas.Update(clinicaBuscada);
+
+            ctx.SaveChanges();
+        }
+
+        public Clinica BuscarPorId(int idClinica)
+        {
+            return ctx.Clinicas.FirstOrDefault(c => c.IdClinica == idClinica);
+        }
+
+        public void Cadastrar(Clinica novaClinica)
+        {
+            ctx.Clinicas.Add(novaClinica);
+
+            ctx.SaveChanges();
+        }
+
+        public void Deletar(int idClinica)
+        {
+            Clinica clinicaBuscada = BuscarPorId(idClinica);
+
+            ctx.Clinicas.Remove(clinicaBuscada);
+
+            ctx.SaveChanges();
+        }
+
+        public List<Clinica> Listar()
+        {
+            return ctx.Clinicas.ToList();
+        }
+    }
+}
---
+++ BACKEND/SPMEDICALGROUP_SENAI_WEBAPI/SPMEDICALGROUP_SENAI_WEBAPI/Repositories/MedicoRepository.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SPMEDICALGROUP_SENAI_WEBAPI.Context;
+using SPMEDICALGROUP_SENAI_WEBAPI.Domains;
+using SPMEDICALGROUP_SENAI_WEBAPI.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SPMEDICALGROUP_SENAI_WEBAPI.Repositories
+{
+    public class MedicoRepository : IMedicoRepository
+    {
+        SPmedContext ctx = new SPmedContext();
+public void Atualizar(Medico MedicoAtualizado)
+        {
+            Medico medicoBuscado = BuscarPorId(MedicoAtualizado.IdMedico);
+
+            if (MedicoAtualizado.IdClinica != 0)
+            {
+                medicoBuscado.IdClinica = MedicoAtualizado.IdClinica;
+            }
+
+            if (MedicoAtualizado.Crm != null)
+            {
+                medicoBuscado.Crm = MedicoAtualizado.Crm;
+            }
+
+
+            if (MedicoAtualizado.IdEspecialidade != 0)
+            {
+                medicoBuscado.IdEspecialidade = MedicoAtualizado.IdEspecialidade;
+            }
+
+            if (medicoBuscado.IdUsuario != 0)
+            {
+                medicoBuscado.IdUsuario = MedicoAtualizado.IdUsuario;
+            }
+
+            ctx.Medicos.Update(medicoBuscado);
+
+            ctx.SaveChanges();
+        }
+
+        public Medico BuscarPorId(int idMedico)
+        {
+            return ctx.Medicos.Include(u => u.IdUsuarioNavigation).Include(c => c.IdClinicaNavigation)
+                .Include(e => e.IdEspecialidadeNavigation).FirstOrDefault(m => m.IdMedico == idMedico);
+
+        }
+
+        public void Cadastrar(Medico novoMedico)
+        {
+            ctx.Medicos.Add(novoMedico);
+
+            ctx.SaveChanges();
+        }
+
+        public void Deletar(int idMedico)
+        {
+            Medico medicoBuscado = BuscarPorId(idMedico);
+
+            ctx.Medicos.Remove(medicoBuscado);
+
+            ctx.SaveChanges();
+        }
+
+        public List<Medico> Listar()
+        {
+            return ctx.Medicos.Include(u => u.IdUsuarioNavigation).Include(c => c.IdClinicaNavigation)
+                .Include(e => e.IdEspecialidadeNavigation).ToList();
+
+        }
+    }
+}
